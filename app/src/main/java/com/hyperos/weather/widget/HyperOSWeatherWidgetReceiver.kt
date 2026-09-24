47f8e9e12a91d475a9235348d6e29b64dd971910
thuanthai01 <thuanseo141@gmail.com>
2026-09-24 10:50:58 +0700

    Update HyperOSWeatherWidgetReceiver.kt

--- app/src/main/java/com/hyperos/weather/widget/HyperOSWeatherWidgetReceiver.kt
+++ app/src/main/java/com/hyperos/weather/widget/HyperOSWeatherWidgetReceiver.kt
@@ -1,5 +1,6 @@
 package com.hyperos.weather.widget
 
+import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 
@@ -7,4 +8,22 @@ class HyperOSWeatherWidgetReceiver : GlanceAppWidgetReceiver() {
 
     override val glanceAppWidget: GlanceAppWidget
         get() = HyperOSWeatherWidget()
+
+    override fun onEnabled(context: Context) {
+        super.onEnabled(context)
+
+        // Bắt đầu cập nhật đồng hồ mỗi phút
+        ClockScheduler.schedule(context)
+
+        // Bắt đầu cập nhật thời tiết
+        WeatherScheduler.schedule(context)
+    }
+
+    override fun onDisabled(context: Context) {
+        super.onDisabled(context)
+
+        // Dừng lịch khi không còn widget
+        ClockScheduler.cancel(context)
+        WeatherScheduler.cancel(context)
+    }
 }
