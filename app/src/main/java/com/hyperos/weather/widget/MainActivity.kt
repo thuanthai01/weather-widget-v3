098eabb8b9256a15f8a30afffb3faf4801ca03e5
thuanthai01 <thuanseo141@gmail.com>
2026-09-24 10:44:05 +0700

    Create MainActivity.kt

---
+++ app/src/main/java/com/hyperos/weather/widget/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.hyperos.weather.widget
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : ComponentActivity() {
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST = 1001
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        checkLocationPermission()
+    }
+
+    private fun checkLocationPermission() {
+
+        val fineLocationGranted =
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+
+        val coarseLocationGranted =
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+
+        if (!fineLocationGranted && !coarseLocationGranted) {
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                LOCATION_PERMISSION_REQUEST
+            )
+
+        } else {
+            finish()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(
+            requestCode,
+            permissions,
+            grantResults
+        )
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST) {
+            finish()
+        }
+    }
+}
