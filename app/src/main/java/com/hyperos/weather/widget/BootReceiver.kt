1a51ba27d131d2b8507d843202988235097b0c61
thuanthai01 <thuanseo141@gmail.com>
2026-09-24 10:48:17 +0700

    Create BootReceiver.kt

---
+++ app/src/main/java/com/hyperos/weather/widget/BootReceiver.kt
@@ -0,0 +1,23 @@
+package com.hyperos.weather.widget
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+
+    override fun onReceive(
+        context: Context,
+        intent: Intent?
+    ) {
+
+        if (intent?.action ==
+            Intent.ACTION_BOOT_COMPLETED
+        ) {
+
+            WeatherScheduler.schedule(
+                context.applicationContext
+            )
+        }
+    }
+}
